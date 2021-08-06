73ae6d6497437d305921c4cbdad8aa7a51520815
Rukhsar Khan <rukhsarmev07@gmail.com>
2021-08-06 17:20:05 +0530

    quizz app

---
+++ src/Pages/Quiz/Quiz.js
@@ -0,0 +1,66 @@
+import { CircularProgress } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import Question from "../../components/Question/Question";
+import "./Quiz.css";
+
+
+const Quiz = ({ name, questions, score, setScore, setQuestions , duration, difficulty}) => {
+  const [options, setOptions] = useState();
+  const [currQues, setCurrQues] = useState(0);
+
+  useEffect(() => {
+    setOptions(
+      questions &&
+        handleShuffle([
+          questions[currQues]?.correct_answer,
+          ...questions[currQues]?.incorrect_answers,
+        ])
+    );
+  }, [currQues, questions]);
+
+  console.log(questions);
+
+  const handleShuffle = (options) => {
+    return options.sort(() => Math.random() - 0.5);
+  };
+
+  return (
+    <div className="quiz">
+      <span className="subtitle">Welcome, {name}</span>
+
+      {questions ? (
+        <>
+          <div className="quizInfo">
+            <span>{questions[currQues].category}</span>
+            <span>
+              {/* {questions[currQues].difficulty} */}
+              Score : {score}
+            </span>
+          </div>
+          <Question
+            currQues={currQues}
+            setCurrQues={setCurrQues}
+            questions={questions}
+            options={options}
+            correct={questions[currQues]?.correct_answer}
+            score={score}
+            setScore={setScore}
+            setQuestions={setQuestions}
+            duration={10}
+            // difficulty={atob(difficulty)}
+          />
+        </>
+      ) : (
+        <CircularProgress
+          style={{ margin: 100 }}
+          color="inherit"
+          size={150}
+          thickness={1}
+        />
+      )}
+    </div>
+  );
+};
+
+
+export default Quiz;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Created By Rukhsar Khan
+        
+      </div>
+    );
+  };
+  
+  export default Footer;
+  
